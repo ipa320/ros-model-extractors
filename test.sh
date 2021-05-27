dbfc5a6506978e54f775e7fbca6b5c93e039f895
ipa-nhg <nadia.hammoudeh.garcia@ipa.fraunhofer.de>
2021-05-27 15:32:33 +0200

    simplify the test script

--- test.sh
+++ test.sh
@@ -3,64 +3,37 @@
 # Arguments:
 #   1: Distro to test
 
-hokuyo_model="PackageSet {
-  CatkinPackage hokuyo_node {
-    Artifact hokuyo_node {
-      Node { name hokuyo_node
-        Publishers {
-          Publisher { name 'scan' message 'sensor_msgs.LaserScan'}}}
-}}}"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
 
-if [ "${1}" = "melodic" ]; then
-  ./haros_runner.sh hokuyo_node hokuyo_node node . /root/ws https://github.com/ros-drivers/hokuyo_node
-  if [ "$(echo "$hokuyo_model" | sed -E 's,\\t|\\r|\\n,,g')" != "$(cat /root/ws/hokuyo_node.ros | sed -E 's,\\t|\\r|\\n,,g')" ] ;then
-    echo "Test failed; the auto-generated model is different to the expected one:"
-    echo "$hokuyo_model"
-    exit 1
-  fi
-fi
-
-s300_model="PackageSet {
-  CatkinPackage cob_sick_s300 {
-    Artifact cob_sick_s300 {
-      Node { name cob_sick_s300
-        Publishers {
-          Publisher { name '/diagnostics' message 'diagnostic_msgs.DiagnosticArray'},
-          Publisher { name 'scan_standby' message 'std_msgs.Bool'},
-          Publisher { name 'scan' message 'sensor_msgs.LaserScan'}}}
-}}}"
-
-if [ "${1}" = "noetic" ]; then
-  ./haros_runner.sh cob_sick_s300 cob_sick_s300 node . /root/ws https://github.com/ipa320/cob_driver
-  if [ "$(echo "$s300_model" | tr -d '[:space:]')" != "$(cat /root/ws/cob_sick_s300.ros | tr -d '[:space:]')" ] ;then
-      :
-  else
-    echo "Test failed; the auto-generated model is different to the expected one:"
-    echo "$s300_model"
-    exit 1
-  fi
-fi
-
-safetyscanner_model="PackageSet {
-  CatkinPackage sick_safetyscanners2 {
-    Artifact sick_safetyscanners2_node {
-      Node { name sick_safetyscanners2_node
+test_model="PackageSet {
+  CatkinPackage test_pkg {
+    Artifact test_node {
+      Node { name test_node
         ServiceServers {
-          ServiceServer { name 'field_data' service 'sick_safetyscanners2_interfaces.FieldData'}}
+          ServiceServer { name 'setBool' service 'std_srvs.SetBool'}}
+        ServiceClients {
+          ServiceClient { name 'init' service 'std_srvs.Trigger'}}
         Publishers {
-          Publisher { name 'scan' message 'sensor_msgs.LaserScan'},
-          Publisher { name 'output_paths' message 'sick_safetyscanners2_interfaces.OutputPaths'},
-          Publisher { name 'raw_data' message 'sick_safetyscanners2_interfaces.RawMicroScanData'},
-          Publisher { name 'extended_scan' message 'sick_safetyscanners2_interfaces.ExtendedLaserScan'}}}
+          Publisher { name 'scan' message 'sensor_msgs.LaserScan'}}
+        Subscribers {
+          Subscriber { name 'power_state' message 'sensor_msgs.BatteryState'}}}
 }}}"
 
-if [ "${1}" = "foxy" ]; then
-  ./haros_runner.sh sick_safetyscanners2 sick_safetyscanners2_node node . /root/ws  https://github.com/SICKAG/sick_safetyscanners2
-  if [ "$safetyscanner_model" == "$(cat /root/ws/sick_safetyscanners2_node.ros)" ] ;then
-      :
-  else
-    echo "Test failed; the auto-generated model is different to the expected one:"
-    echo "$safetyscanner_model"
-    exit 1
-  fi
+./haros_runner.sh test_pkg test_node node . /root/ws https://github.com/ipa-nhg/test_ros_code_extractor "${1}"
+
+
+if [ "$(echo "$test_model" | sed -E 's,\\t|\\r|\\n,,g')" != "$(cat /root/ws/test_node.ros | sed -E 's,\\t|\\r|\\n,,g')" ] ;then
+  echo -e "${RED} [TEST] EXTRACTION FAILS"
+  echo -e "${RED} EXTRACTED AND EXPECTED MODEL DON'T MATCH"
+  echo "The expected model is:"
+  echo "~~~"
+  echo "$test_model"
+  echo "~~~"
+  exit 1
+else
+  echo -e "${GREEN} [TEST] BUILD SUCCESSFUL"
+  echo -e "${GREEN} [TEST] EXTRACTION SUCCESSFUL"
+  echo -e "${GREEN} [TEST] COMPARISON OF EXTRACTED AND EXPECTED MODEL SUCCESSFUL"
+  exit 0
 fi
