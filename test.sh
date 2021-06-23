8a42df7187c9c540ade3f5efc0373cc19eb9f6eb
ipa-nhg <nadia.hammoudeh.garcia@ipa.fraunhofer.de>
2021-06-23 10:09:56 +0200

    update test script to allow unordered interfaces

--- test.sh
+++ test.sh
@@ -27,20 +27,36 @@ test_model="PackageSet {
           Parameter { name 'struc_test/first_element' type Integer }}}
 }}}"
 
+declare -a InterfacesArray=("ServiceServer { name 'setBool' service 'std_srvs.SetBool'}"
+"ServiceClient { name 'init' service 'std_srvs.Trigger'}"
+"Publisher { name 'scan' message 'sensor_msgs.LaserScan'}"
+"Subscriber { name 'power_state' message 'sensor_msgs.BatteryState'}"
+"Parameter { name 'string_test' type String value 'test'}"
+"Parameter { name 'bool_tets' type Boolean }"
+"Parameter { name 'int_test' type Integer }"
+"Parameter { name 'struc_test/last_element/hola' type Integer }"
+"Parameter { name 'double_test' type Double }"
+"Parameter { name 'struc_test/first_element' type Integer }")
+
 ./haros_runner.sh test_pkg test_node node . /root/ws "https://github.com/ipa-nhg/test_ros_code_extractor -b ${1}"
 
+resulted_model=$(cat /root/ws/test_node.ros | sed -E 's,\\t|\\r|\\n,,g')
 
-if [ "$(echo "$test_model" | sed -E 's,\\t|\\r|\\n,,g')" != "$(cat /root/ws/test_node.ros | sed -E 's,\\t|\\r|\\n,,g')" ] ;then
-  echo -e "${RED} [TEST] EXTRACTION FAILS"
-  echo -e "${RED} EXTRACTED AND EXPECTED MODEL DON'T MATCH"
-  echo "The expected model is:"
-  echo "~~~"
-  echo "$test_model"
-  echo "~~~"
-  exit 1
-else
-  echo -e "${GREEN} [TEST] BUILD SUCCESSFUL"
-  echo -e "${GREEN} [TEST] EXTRACTION SUCCESSFUL"
-  echo -e "${GREEN} [TEST] COMPARISON OF EXTRACTED AND EXPECTED MODEL SUCCESSFUL"
-  exit 0
-fi
+for interface in "${InterfacesArray[@]}"
+do
+  if ! echo "$resulted_model" | grep -q "$interface" 
+  then
+    echo -e "${RED} [TEST] EXTRACTION FAILS"
+    echo -e "${RED} EXTRACTED AND EXPECTED MODEL DON'T MATCH"
+    echo "The following interface couldn't be found:"
+    echo "~~~"
+    echo "$interface"
+    echo "~~~"
+    exit 1
+  fi
+done
+
+echo -e "${GREEN} [TEST] BUILD SUCCESSFUL"
+echo -e "${GREEN} [TEST] EXTRACTION SUCCESSFUL"
+echo -e "${GREEN} [TEST] COMPARISON OF EXTRACTED AND EXPECTED MODEL SUCCESSFUL"
+exit 0
