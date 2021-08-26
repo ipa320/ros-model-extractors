b9fcdcd97707cdb51dd181f28b073afc8c6a7cb4
ipa-nhg <nadia.hammoudeh.garcia@ipa.fraunhofer.de>
2021-08-26 13:59:38 +0200

    add scripts to generate the models for the specifications

--- haros_runner.sh
+++ haros_runner.sh
@@ -23,17 +23,19 @@ done
 
 cd "${5}"
 model_repo=$(echo "${6}" | sed 's/ .*//')
+source devel/setup.bash
+source install/setup.bash
 
 echo ""
 echo "## Install ROS pkgs dependencies ##"
 if [ -n $ROS_VERSION ]
 then
-  if [ $ROS_VERSION == "1" ]
+  if [[ $ROS_VERSION == "1" ]]
   then
     source devel/setup.bash
     rosdep install -y -i -r --from-path src
     catkin_make -DCMAKE_EXPORT_COMPILE_COMMANDS=1
-  elif [ $ROS_VERSION == "2" ]
+  elif [[ $ROS_VERSION == "2" ]]
   then
     source install/setup.bash
     rosdep install -y -i -r --from-path src
@@ -109,3 +111,6 @@ echo ""
 echo "~~~~~~~~~~~"
 echo "###########"
 done
+
+## Clean and finish
+rm -rf ${5}/src/*
