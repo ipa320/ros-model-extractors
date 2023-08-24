760b1aa9ce9aaaf86ba229b8ad788de95e25093b
Divya Tyagi <tyagi.divya5@gmail.com>
2023-08-25 01:14:27 +0200

    delete unnecessary folders

--- build/turtlesim/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/divya/Master_Thesis/RosModel_Extractor_fork/ros-model-extractors/ros_tutorials/turtlesim
-# Build directory: /home/divya/Master_Thesis/RosModel_Extractor_fork/ros-model-extractors/build/turtlesim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("turtlesim__py")
--- build/turtlesim/turtlesim__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/divya/Master_Thesis/RosModel_Extractor_fork/ros-model-extractors/build/turtlesim/turtlesim__py
-# Build directory: /home/divya/Master_Thesis/RosModel_Extractor_fork/ros-model-extractors/build/turtlesim/turtlesim__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
